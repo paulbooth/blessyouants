6315f6433822bf260e9403add4c3679f538946b5
Paul Booth <thephantompaulbooth@gmail.com>
2013-01-21 21:47:41 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,37 @@
+/**
+ * Module dependencies.
+ */
+
+var express = require('express')
+  , http = require('http')
+  , path = require('path');
+
+var app = express();
+
+app.configure(function(){
+  app.set('port', process.env.PORT || 3000);
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'jade');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(app.router);
+  app.use('/public', express.static(path.join(__dirname, 'public')));
+});
+
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+app.get('/', function(req, res) {
+  res.render('blants.jade', {word:'Paul Booth'});
+});
+
+app.get('/:word', function(req, res) {
+  res.render('blants.jade', {word:req.params.word})
+});
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log("Express server listening on port " + app.get('port'));
+});
